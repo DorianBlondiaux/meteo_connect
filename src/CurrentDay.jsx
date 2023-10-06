62cbc9f48664df42cea0089d3dd36bcf9f33544f
Huluette <leana.beuvant@gmail.com>
2023-10-06 16:12:32 +0200

    Ajout des lignes d'affichage des données

--- src/CurrentDay.jsx
+++ src/CurrentDay.jsx
@@ -11,14 +11,12 @@ function CurrentDay(props) {
     <div className="card text-center m-3">
       <h5 className="card-header">Simple GET Request</h5>
       <div className="card-body">
-        <li>{props.myMeteo.latitude}</li>
-        <li>{props.myMeteo.longitude}</li>
         <li>{props.myMeteo.daily.time[0]}</li>
         <li>{props.myMeteo.daily.weathercode[0]}</li>
-        <li>{props.myMeteo.daily.apparent_temperature_max[0]}</li>
-        <li>{props.myMeteo.daily.apparent_temperature_min[0]}</li>
-        <li>{props.myMeteo.daily.precipitation_sum[0]}</li>
-        <li>{props.myMeteo.daily.windspeed_10m_max[0]}</li>
+        <li>{props.myMeteo.daily.apparent_temperature_max[0]}{props.myMeteo.daily_units.apparent_temperature_max}</li>
+        <li>{props.myMeteo.daily.apparent_temperature_min[0]}{props.myMeteo.daily_units.apparent_temperature_min}</li>
+        <li>{props.myMeteo.daily.precipitation_sum[0]}{props.myMeteo.daily_units.precipitation_sum}</li>
+        <li>{props.myMeteo.daily.windspeed_10m_max[0]}{props.myMeteo.daily_units.windspeed_10m_max}</li>
         <li>{props.myMeteo.daily.sunrise[0]}</li>
         <li>{props.myMeteo.daily.sunset[0]}</li>
       </div>
