ce418f812952839bd8e4a3ce24c4fe1cf22410bf
Huluette <leana.beuvant@gmail.com>
2023-10-06 15:30:49 +0200

    récupération de l'affichage des données daily

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -31,7 +31,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
 
         <CurrentDay myMeteo={myMeteo} isLoading={isLoading}></CurrentDay>
       </header>
--- src/CurrentDay.jsx
+++ src/CurrentDay.jsx
@@ -4,22 +4,28 @@ import { useState, useEffect } from "react";
 
 function CurrentDay(props) {
 
-      if (props.isLoading) {
-        return <div className="App">Loading...</div>;
-      }
-        return (
-            <div className="card text-center m-3">
-                <h5 className="card-header">Simple GET Request</h5>
-                <div className="card-body">
-                    Latitude:
-                    { <li>{props.myMeteo.latitude}</li> }
-                    Longitude:
-                    { <li>{props.myMeteo.longitude}</li> }
-                </div>
-            </div>
-        );
-    
+  if (props.isLoading) {
+    return <div className="App">Loading...</div>;
   }
+  return (
+    <div className="card text-center m-3">
+      <h5 className="card-header">Simple GET Request</h5>
+      <div className="card-body">
+        <li>{props.myMeteo.latitude}</li>
+        <li>{props.myMeteo.longitude}</li>
+        <li>{props.myMeteo.daily.time[0]}</li>
+        <li>{props.myMeteo.daily.weathercode[0]}</li>
+        <li>{props.myMeteo.daily.apparent_temperature_max[0]}</li>
+        <li>{props.myMeteo.daily.apparent_temperature_min[0]}</li>
+        <li>{props.myMeteo.daily.precipitation_sum[0]}</li>
+        <li>{props.myMeteo.daily.windspeed_10m_max[0]}</li>
+        <li>{props.myMeteo.daily.sunrise[0]}</li>
+        <li>{props.myMeteo.daily.sunset[0]}</li>
+      </div>
+    </div>
+  );
 
-  
+}
+
+
 export default CurrentDay;
