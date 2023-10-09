b114fe1b8477c32325d24b791cbf777408e65d5d
mdemazur <mdemazur@student.42.fr>
2023-10-09 14:49:28 +0200

    style CurrentDay

--- src/CurrentDay.jsx
+++ src/CurrentDay.jsx
@@ -8,14 +8,14 @@ function CurrentDay(props) {
   return (
     <div className="card text-center m-3">
       <h5 className="card-header">Simple GET Request</h5>
-      <div className="card-body">
-        <li>{getDateDay(props.myMeteo.daily.time[0])}, Today</li>
-        <li>{props.myMeteo.daily.apparent_temperature_max[0]} {props.myMeteo.daily_units.apparent_temperature_max}</li>
-        <li>{props.myMeteo.daily.apparent_temperature_min[0]} {props.myMeteo.daily_units.apparent_temperature_min}</li>
-        <li>{props.myMeteo.daily.precipitation_sum[0]} {props.myMeteo.daily_units.precipitation_sum}</li>
-        <li>{props.myMeteo.daily.windspeed_10m_max[0]} {props.myMeteo.daily_units.windspeed_10m_max}</li>
-        <li>{sunrise.getHours()}:{sunrise.getMinutes()}</li>
-        <li>{sunset.getHours()}:{sunset.getMinutes()}</li>
+      <div className="card-body-today">
+        <li className="today">{getDateDay(props.myMeteo.daily.time[0])}, Today</li>
+        <li className="temperature_max"> {props.myMeteo.daily.apparent_temperature_max[0]} {props.myMeteo.daily_units.apparent_temperature_max}</li>
+        <li className="temperature_min"> {props.myMeteo.daily.apparent_temperature_min[0]} {props.myMeteo.daily_units.apparent_temperature_min}</li>
+        <li className="precipitation"> {props.myMeteo.daily.precipitation_sum[0]} {props.myMeteo.daily_units.precipitation_sum}</li>
+        <li className="windspeed"> {props.myMeteo.daily.windspeed_10m_max[0]} {props.myMeteo.daily_units.windspeed_10m_max}</li>
+        <li className="sunrise"> {sunrise.getHours()}:{sunrise.getMinutes()}</li>
+        <li className="sunset"> {sunset.getHours()}:{sunset.getMinutes()}</li>
       </div>
     </div>
   );
