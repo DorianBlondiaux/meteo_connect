7cb1308cdbd8deb1e7f2a847b8d7f755719d4710
Blondiaux Dorian <dorian5959@gmail.com>
2023-10-10 14:20:07 +0200

    Supress dead code

--- src/App.js
+++ src/App.js
@@ -8,27 +8,39 @@ function App() {
 
   const [isLoading, setLoading] = useState(true);
   const [myMeteo, setMyMeteo] = useState();
+
+  const [isLoadingHourly, setLoadingHourly] = useState(true);
+  const [myMeteoHourly, setMyMeteoHourly] = useState();
+
   const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=50.6942&longitude=3.1746&daily=weathercode,apparent_temperature_max,apparent_temperature_min,precipitation_sum,windspeed_10m_max,sunrise,sunset&timezone=Europe%2FLondon';
+  const apiUrlHourly = 'https://api.open-meteo.com/v1/forecast?latitude=50.6942&longitude=3.1746&hourly=apparent_temperature,weathercode&forecast_days=1&timezone=Europe%2FLondon';
+
 
   useEffect(() => {
     axios.get(apiUrl).then(response => {
       setMyMeteo(response.data);
       setLoading(false);
     });
+
+    axios.get(apiUrlHourly).then(response => {
+      setMyMeteoHourly(response.data);
+      setLoadingHourly(false);
+    });
+    console.log(myMeteoHourly)
   }, []);
 
-  if (isLoading) {
+  if (isLoading && isLoadingHourly) {
     return <div className="App">Loading...</div>;
   }
 
   return (
     <div className="App">
-      <CurrentDay myMeteo={myMeteo} isLoading={isLoading}></CurrentDay>
+      <CurrentDay myMeteo={myMeteo}></CurrentDay>
       <p>Next days weather:</p>
       <div className="Weather">
         {myMeteo.daily.time.slice(1).map((day, i) => (
           <NextDays key={i} time={day} temperature_max={myMeteo.daily.apparent_temperature_max[i + 1]}
-            isLoading={isLoading} tempUnit={myMeteo.daily_units.apparent_temperature_max}
+            tempUnit={myMeteo.daily_units.apparent_temperature_max}
             weatherCode={myMeteo.daily.weathercode[i + 1]}></NextDays>
         ))}
       </div>
--- src/NextDays.jsx
+++ src/NextDays.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import getDateDay from "./Utility/GetDateDay";
 import getWeatherIcone from "./Utility/GetWeatherIcone";
 function NextDays(props) {
-
-    if (props.isLoading) {
-      return <div className="App">Loading...</div>;
-    }
+  
     return (
       <div className="card text-center m-3">
         <div className="card-body hoverbody">
